ea3734a5cdc2a92cc7fcd76908b8560a4bd14979
Joshua Firme <61042416+joshuafirme@users.noreply.github.com>
2020-11-14 10:32:35 +0800

    Update CompanyMaintenance.php

--- app/CompanyMaintenance.php
+++ app/CompanyMaintenance.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class CompanyMaintenance extends Model
 {
+    protected $table = 'tblcompany';
     protected $fillable = ['id', 'company_name', 'markup'];
 }
